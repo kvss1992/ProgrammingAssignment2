e75128f7e9c5a13f39ebb62a5a98098c1fcd3011
Karan <Kvss1992@gmail.com>
2016-03-16 04:18:07 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,8 @@
 # 2. get the value of the matrix
 # 3. set the value of inverse of the matrix
 # 4. get the value of inverse of the matrix
-
 makeCacheMatrix <- function(x = matrix()) {
-	inv <- NULL
+    inv <- NULL
     set <- function(y) {
         x <<- y
         inv <<- NULL
@@ -21,11 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
 
+# This function assumes that the matrix is always invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-	inv <- x$getinverse()
+    inv <- x$getinverse()
     if(!is.null(inv)) {
         message("getting cached data.")
         return(inv)
@@ -35,3 +37,25 @@ cacheSolve <- function(x, ...) {
     x$setinverse(inv)
     inv
 }
+
+## Sample run:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(m)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
